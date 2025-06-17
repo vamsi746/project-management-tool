87f85b195e87ed1cf84423ab691ceddbd700636a
vamsi746 <sangarajuvamsi6@gmail.com>
2025-06-17 12:02:37 +0530

    Fix axiosInstance redeclaration and circular import

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,17 +1,23 @@
 // src/utils/axiosInstance.js
-import axiosInstance from '../utils/axiosInstance';
-
+import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://project-management-tool-wtmq.onrender.com',
-  withCredentials: true, // if using cookies
+  withCredentials: true, // optional: for cookies/session if used
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
-// Automatically attach token
+// Automatically attach token if present
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default axiosInstance;
