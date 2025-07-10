5968fcedd9cd9ad0beaa50426179a47e59726010
Lakshmi Narayana <sangarajuvamsi6@gmail.com>
2025-07-10 16:06:52 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,15 +8,15 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+// userSchema.pre('save', async function (next) {
+//  if (!this.isModified('password')) return next();
+ // try {
+ //   const salt = await bcrypt.genSalt(10);
+ //   this.password = await bcrypt.hash(this.password, salt);
+//    next();
+//  } catch (err) {
+//    next(err);
+//  }
+// });
 
 module.exports = mongoose.model('User', userSchema);
