53c2ab1dcc07344db51660610d626139d7fd9f18
Lakshmi Narayana <sangarajuvamsi6@gmail.com>
2025-07-10 16:57:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,9 @@ const authRoutes = require('./routes/auth');
 // ✅ CORS configuration
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://project-management-tool-h5xa8hhrm-vamsi746s-projects.vercel.app',
+  'https://project-management-tool-delta-rosy.vercel.app/',
   'https://project-management-tool-pszc1hloj-vamsi746s-projects.vercel.app',
   'https://project-management-tool-k4sdn3hnx-vamsi746s-projects.vercel.app',
-  'https://project-management-tool-delta-rosy.vercel.app'
 ];
 
 app.use(
